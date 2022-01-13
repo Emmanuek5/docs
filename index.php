5e2b5de317acf84c5f7d822515ab710c6f69b19b
Emmanuek5 <83721168+Emmanuek5@users.noreply.github.com>
2022-01-13 23:05:39 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://w3css.blueobsidian.repl.co/w3css/w3.css">
+    <link rel="icon" href="./favicon.ico">
+    <title>Rtis Computer Club</title>
+</head>
+
+<body>
+    <header class="w3-header">
+
+    </header>
+</body>
+
+</html>
